d200e3941a831e295b18ed59dd461714b0163be7
Shaun Yap <me@shaunyap.ca>
2020-11-18 04:37:32 +0000

    Add AJAX get to load tweets from db

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,8 +4,8 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 $(document).ready(function() {
-  renderTweets(data);
   submitTweet();
+  loadTweets();
 })
 
 const renderTweets = function(tweets) {
@@ -56,6 +56,13 @@ const submitTweet = function() {
   })
 }
 
+const loadTweets = function() {
+  $.ajax('/tweets', { method: 'GET' })
+  .then(function(data) {
+    renderTweets(data);
+  })
+}
+
 function getDatePosted(datetime) {
   const d = new Date();
   const datePosted = new Date(datetime);
@@ -63,28 +70,4 @@ function getDatePosted(datetime) {
   return Math.round(timeDifference / (1000 * 3600 * 24));
 }
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
 
